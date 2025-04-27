e5f7349d9f75a04c492776e690b90a769f9c54ab
Fayro1999 <anosikesomadina8@gmail.com>
2025-04-27 21:10:23 +0100

    updated settings

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,27 +1,24 @@
-import random 
+import random
 from rest_framework import serializers
 from .models import Transaction
 from land.serializers import LandSerializer
 from land.models import Land
 
 class TransactionSerializer(serializers.ModelSerializer):
-   land = serializers.PrimaryKeyRelatedField(queryset=Land.objects.all())
+    land = serializers.PrimaryKeyRelatedField(queryset=Land.objects.all())
 
-class Meta:
+    class Meta:
         model = Transaction
         fields = [
-           'land', 'buyer_name', 'buyer_email', 'buyer_phone',
+            'land', 'buyer_name', 'buyer_email', 'buyer_phone',
             'amount', 'amount_paid', 'balance', 'status', 'date',
-            'time', 'payment_method', 'transaction_id', 'created_by','id'
+            'time', 'payment_method', 'transaction_id', 'created_by', 'id'
         ]
         read_only_fields = ['transaction_id']  # These fields are auto-generated
 
-def create(self, validated_data):
+    def create(self, validated_data):
         # Automatically set the 'created_by' field to the authenticated user
         user = self.context['request'].user  # Assuming the user is authenticated
         validated_data['created_by'] = user
         validated_data['transaction_id'] = f"AHE{random.randint(100000, 99999999)}"
         return super().create(validated_data)
-
-   
-    
