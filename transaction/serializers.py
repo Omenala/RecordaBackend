c6d69e49a46cabc3358c4b0eb84f9f74898da745
Fayro1999 <anosikesomadina8@gmail.com>
2025-04-30 00:22:12 +0100

    updated settings

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -5,24 +5,21 @@ from land.serializers import LandSerializer
 from land.models import Land
 
 class TransactionSerializer(serializers.ModelSerializer):
-    land_id = serializers.PrimaryKeyRelatedField(queryset=Land.objects.all(),  write_only=True)
+    land_id = serializers.PrimaryKeyRelatedField(queryset=Land.objects.all(), write_only=True)
     land = LandSerializer(read_only=True)
 
     class Meta:
         model = Transaction
         fields = [
-            'land', 'buyer_name', 'buyer_email', 'buyer_phone',
+            'land', 'land_id', 'buyer_name', 'buyer_email', 'buyer_phone',
             'amount', 'amount_paid', 'balance', 'status', 'date',
             'time', 'payment_method', 'transaction_id', 'created_by', 'id'
         ]
-        read_only_fields = ['transaction_id']  # These fields are auto-generated
+        read_only_fields = ['transaction_id', 'created_by']
 
-     
-
-        # Automatically set the 'created_by' field to the authenticated user
-        user = self.context['request'].user  # Assuming the user is authenticated
+    def create(self, validated_data):
+        user = self.context['request'].user
         validated_data['created_by'] = user
         validated_data['transaction_id'] = f"AHE{random.randint(100000, 99999999)}"
-        validated_data['land'] = land  # Assign the created Land instance
-
+        validated_data['land'] = validated_data.pop('land_id')  # map write-only field to FK
         return super().create(validated_data)
